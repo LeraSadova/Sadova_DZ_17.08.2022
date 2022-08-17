65a461390df8d845b373a8bbd541e9ebef781490
Садова Валерия Андреевна <SadovaVA@vseinstrumenti.ru>
2022-08-17 13:28:43 +0300

    решила 29ю задачку

--- Ex_29/Program.cs
+++ Ex_29/Program.cs
@@ -5,8 +5,9 @@ Console.WriteLine("ps: лучше введите небольшое число, 
 
 int n=Convert.ToInt32(Console.ReadLine());
 int[] array_new = new int[n]; //мы создаем массив на n элементов
-
 int i=0;
+if (n<=10)
+{
 while (i<n)
 {
     Console.WriteLine($"Введите {i+1}-й элемент массива"); //вводим элементы массива с клавиатуры
@@ -14,7 +15,16 @@ while (i<n)
     //Console.WriteLine();
     i++;
 }
-
+}
+else 
+{
+    Console.WriteLine("Число элементов массива больше 10, программа заполнит его рандомно, ок?");
+    while (i<n)
+    {
+   
+   array_new[i]=new Random().Next(0, 20);    
+    i++;}
+}
 //выводим массив на экран:
 for (i = 0; i < n; i = i + 1)
 Console.Write($"[{array_new[i]}]");
